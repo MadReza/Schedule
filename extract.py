7ab1f799cc638c05716a1d335f2f6ea2d6651e62
Reza Madabadi <rezmaza@gmail.com>
2018-12-25 11:04:56 -0500

    Extraction complete from Word.

--- extract.py
+++ extract.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import calendar
+from time import strptime, strftime, mktime
+import re   #text splitting
 from xml.etree.ElementTree import XML
 import zipfile
 
@@ -24,9 +27,56 @@ def extract_text(element):
         text += " "
     return text.strip()
 
+def convert_str_date(txt):
+    datetime_object = datetime.strptime("" + str(datetime.now().year) + " " + txt, '%Y %B %d') #https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+    if datetime_object.month < datetime.now().month:
+        year = datetime.now().year + 1
+        datetime_object = datetime(year=year, month=datetime_object.month, day=datetime_object.day)
+    return datetime_object
+
+def extract_date(element):
+    dates = []
+    for paragraph in element.iter(PARA):
+        text = ""
+        for i in paragraph.itertext():
+            text += i
+        split = re.split('; |, | ', text.strip())
+
+        for nums in split[1:]:
+            d = split[0] + " " + nums
+            dates.append(convert_str_date(d))
+            #dates.append(split[0] + " " +  nums)
+    return dates
+
+def extract_time(element):
+    times = {}
+    t = extract_text(element)
+    split = re.split('; |to|\xa0|:| ', t.strip())
+    split = list(filter(None, split)) #Remove Empty from list
+    if len(split) == 0:
+        return times
+
+    #TODO: do checks for the time to be more exact. As in 8 pm and 8 am....
+    if int(split[0]) < 8 or int(split[0]) == 12:
+        start = strptime(split[0] + ":" + split[1] + " PM", "%I:%M %p")
+    else:
+        start = strptime(split[0] + ":" + split[1] + " AM", "%I:%M %p")
+    if int(split[2]) < 8 or int(split[2]) == 12:
+        end = strptime(split[2] + ":" + split[3] + " PM", "%I:%M %p")
+    else:
+        end = strptime(split[2] + ":" + split[3] + " AM", "%I:%M %p")
+    times['start'] = start
+    times['end'] = end
+    #times['length'] = end.time() - start.time()
+    #print(times['length'])
+    sh = start.tm_hour * 60 + start.tm_min
+    eh = end.tm_hour * 60 + end.tm_min
+    times['length'] = eh-sh
+    return times
+
 def extract_table_data(path):
     """
-    Take the path of a docx file as argument, return the text in unicode.
+    Take the path of a docx file as argument, return a list of times in dictionary format. teacher is the key
     """
     document = zipfile.ZipFile(path)
     xml_content = document.read('word/document.xml')
@@ -36,36 +86,30 @@ def extract_table_data(path):
     rows = table.findall(ROW)
 
     day = ""
-    date = ""
+    dates = ""
+    teachers = {}
 
-    for row in rows:
+    for row in rows[1:]:
         cols = row.findall(COL)
 
         #Code
         code = extract_text(cols[0])
-        #print(code)
 
         #course
         course = extract_text(cols[1])
-        #print(course)
 
         #day
         dayTemp = extract_text(cols[2])
         if dayTemp != "":
-            #datetime_object = datetime.strptime(dayTemp, '%b %d %Y %I:%M%p')
             day = dayTemp
-            print(day)
-            #print(datetime_object)
-        #print(day)
 
         #Calendar/Date
-        dateTemp = extract_text(cols[3])
-        if dateTemp != "":
-            date = dateTemp
-        #print(date)
+        temp_date = extract_date(cols[3])
+        if len(temp_date) != 0:
+            dates = temp_date
 
         #hour
-        hour = extract_text(cols[4])
+        times = extract_time(cols[4])
         #print(hour)
 
         #Teachers
@@ -75,11 +119,21 @@ def extract_table_data(path):
         if teacher == "":
             continue
 
-        print(code, course, day, date, hour, teacher)
+        if teacher not in teachers:
+            teachers[teacher] = []
 
-    print(len(rows))
-    print("$$$$$$$$$$$")
+        segment = {}
+        segment['code'] = code
+        segment['course'] = course
+        segment['day'] = day
+        segment['dates'] = dates
+        segment['times'] = times
 
-    return '\n\n'.join("paragraphs")
+        teachers[teacher].append(segment)
+
+
+    print(teachers)
+
+    return teachers
 
 tree = extract_table_data("Schedule.docx"); #Issues with full path .....
